528315da01795ca1368ba8f81e376fbda0326f2c
Mehrab Uddin <mehrab.uddin905@gmail.com>
2021-10-25 15:50:46 +0500

    edit products section

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,7 +1,19 @@
-const ProductFeed = () => {
+import Product from "./Product";
+
+const ProductFeed = ({ products }) => {
   return (
-    <div>
-      <h1>products goes here</h1>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 '>
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
 
-export default function Index() {
+export default function Index({ products }) {
   return (
     <div className='bg-gray-100'>
       <Head>
@@ -15,8 +15,20 @@ export default function Index() {
         <Banner />
 
         {/* ProductFeed */}
-        <ProductFeed />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
